579ef7c11329e4cb273f50891ee6b6441b1c2a02
Akbashev <akbashevaleh@gmail.com>
2024-01-07 16:01:19 +0000

    deploy taski

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,24 @@
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.82.194', '127.0.0.1','localhost','tttaski.hopto.org']
 
+sentry_sdk.init(
+    dsn="https://f9952b9376dcf63605cdf3e7cb548c81@o4506445827735552.ingest.sentry.io/4506445836648448",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
 
 # Application definition
 
@@ -103,7 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-backend/'
+STATIC_ROOT = BASE_DIR / 'static-backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
